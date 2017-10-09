c71a9067987b69e5e23f4755fcdaf60d991567e0
Yordan Georgiev <yordan.georgiev.ext@nokia.com>
2017-10-09 12:09:09 +0300

    add scala-cheat-sheet.sh

---
+++ doc/cheat-sheets/scala/scala-cheat-sheet.sh
@@ -0,0 +1,21 @@
+# file: scala-cheat-sheet.sh
+
+# unless otherwise stated you should always execute all the commands from
+cd <<proj_root_dir>>
+
+# how-to compile my scala project 
+sbt compile
+
+# how-to execute the tests in my scala project 
+sbt test 
+
+# test only a certain test from the 
+sbt "test:test-only *ReaderFactoryTest"
+
+
+
+
+
+# Purpose
+# to provide 
+# eof file: scala-cheat-sheet.sh
