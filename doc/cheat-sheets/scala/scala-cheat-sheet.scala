103daa728ed5a3fec6037c432c95af966c42aff8
Yordan Georgiev <yordan.georgiev@gmail.com>
2017-12-12 21:40:53 +0200

    add scala fold left and fake enum examples

--- doc/cheat-sheets/scala/scala-cheat-sheet.scala
+++ doc/cheat-sheets/scala/scala-cheat-sheet.scala
@@ -13,26 +13,26 @@
 }
 
 // how-to declare hc array
-val a = Array("apple", "banana", "orange")
-val newArray = for (e <- a) yield e.toUpperCase
+  val a = Array("apple", "banana", "orange")
+  val newArray = for (e <- a) yield e.toUpperCase
 
 // how-to create immutable maps 
-val mapStates = Map("AL" -> "Alabama", "AK" -> "Alaska")
-// how-to create a mutable map
-var mapStates = collection.mutable.Map("AL" -> "Alabama")
-var mapStates = collection.mutable.Map[String, String]()
-mapStates += ("AL" -> "Alabama")
+  val mapStates = Map("AL" -> "Alabama", "AK" -> "Alaska")
+  // how-to create a mutable map
+  var mapStates = collection.mutable.Map("AL" -> "Alabama")
+  var mapStates = collection.mutable.Map[String, String]()
+  mapStates += ("AL" -> "Alabama")
 
-val objList :List[Int] = List(1,2,3)
+  val lstInts :List[Int] = List(1,2,3)
 
 // START how-to use Option , None , Some in Scala
-toInt(someString) match {
-    case Some(i) 	=> println(i)
-    case None 		=> println("That didn't work.")
-}
+  toInt(someString) match {
+      case Some(i) 	=> println(i)
+      case None 		=> println("That didn't work.")
+  }
 // STOP how-to use Option , None , Some in Scala
 
-or even better use the Either idiom
+// or even better use the Either idiom
 
 // START how-to use Either
   divideXByY(1, 0) match {
@@ -44,21 +44,43 @@ or even better use the Either idiom
    * A simple method to demonstrate how to declare that a method returns an Either,
    * and code that returns a Left or Right.
    */
-  def divideXByY(x: Int, y: Int): Either[String, Int] = {
-      if (y == 0) 
-      	Left("Dude, can't divide by 0")
-      else 
-      	Right(x / y)
-  }
+    def divideXByY(x: Int, y: Int): Either[String, Int] = {
+        if (y == 0) 
+        	Left("Dude, can't divide by 0")
+        else 
+        	Right(x / y)
+    }
 
 // STOP how-to use Either
 
 
 // create hc dataframe
-var input1 = spark.createDataFrame(Seq(
-(10L, "Joe Doe", 34),
-(11L, "Jane Doe", 31),
-(12L, "Alice Jones", 25)
-)).toDF("id", "name", "age")
+  var input1 = spark.createDataFrame(Seq(
+  (10L, "Joe Doe", 34),
+  (11L, "Jane Doe", 31),
+  (12L, "Alice Jones", 25)
+  )).toDF("id", "name", "age")
+// stop create hs dataframe
+
+//start how-to use case objects instead of Enumerations
+    sealed trait FakeEnumeration { def key: String; def value: String }
+
+    case object fakeEnumKey1 extends FakeEnumeration {
+      val key = "key-01"; val value = "value-01"
+    }
+    case object fakeEnumKey2 extends FakeEnumeration {
+      val key = "key-01"; val value = "value-02"
+    }
+// stop 
+
+// START foldLeft usage 
+val outDf: DataFrame = lstColumnsToIterate
+      .foldLeft(inDf)((tmpDf, iterableColToAdd) => {
+        tmpDf.withColumn(iterableColToAdd,expr(funcToApply).as(iterableColToAdd))
+      })
+      .groupBy(lstGroupByCols.distinct.head, lstGroupByCols.distinct.tail: _*)
+      .agg(lstAggregationCols.distinct.head, lstAggregationCols.distinct.tail: _*)
+// STOP foldLeft usage
+
 
 // eof file: scala-cheat-sheet.scala
